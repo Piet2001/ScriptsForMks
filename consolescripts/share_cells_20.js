f02acb46e619d2366ea24cbebd5d38d75c56a4d0
Piet2001 <piet2001@fireman.net>
2021-09-15 11:35:54 +0200

    share cell scripts

--- consolescripts/share_cells_20.js
+++ consolescripts/share_cells_20.js
@@ -3,13 +3,11 @@ buildings = buildings.filter(b => b.building_type === 5)
 let open = [];
 let costs = [];
 buildings.forEach(b => {
-    if (b.is_alliance_shared) open.push(b.id)
+    if (!b.is_alliance_shared) open.push(b.id)
+    if (!b.is_alliance_shared || b.alliance_share_credits_percentage !== 20) costs.push(b.id)
 })
-
-procesOpenCosts()
-
 async function procesOpenCosts() {
-    let ID = open.length !== 0 ? open[0] : 0
+    let ID = open.length !== 0 ? open[0] : costs.length !== 0 ? costs[0] : 0
     console.log(open.length)
     if (ID === 0) return;
     else if (open.length !== 0) {
@@ -18,4 +16,11 @@ async function procesOpenCosts() {
             procesOpenCosts();
         })
     }
+    else {
+        await fetch(`/buildings/${ID}/alliance_costs/2`).then(() => {
+            costs.splice(0, 1)
+            procesOpenCosts();
+        })
+    }
 }
+procesOpenCosts()
--- consolescripts/share_cells_40.js
+++ consolescripts/share_cells_40.js
@@ -3,18 +3,25 @@ buildings = buildings.filter(b => b.building_type === 5)
 let open = [];
 let costs = [];
 buildings.forEach(b => {
-    if (b.is_alliance_shared) open.push(b.id)
+    if (!b.is_alliance_shared) open.push(b.id)
+    if (!b.is_alliance_shared || b.alliance_share_credits_percentage !== 40) costs.push(b.id)
 })
 
 procesOpenCosts()
 
 async function procesOpenCosts() {
-    let ID = open.length !== 0 ? open[0] : 0
+    let ID = open.length !== 0 ? open[0] : costs.length !== 0 ? costs[0] : 0
     console.log(open.length)
     if (ID === 0) return;
     else if (open.length !== 0) {
         await fetch(`/buildings/${ID}/alliance`).then(() => {
             open.splice(0, 1)
+            procesOpenCosts();
+        })
+    }
+    else {
+        await fetch(`/buildings/${ID}/alliance_costs/4`).then(() => {
+            costs.splice(0, 1)
             procesOpenCosts();
         })
     }
