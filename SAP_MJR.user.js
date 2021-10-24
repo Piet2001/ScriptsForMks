480b873352a28acca236bddd351edbcb34f08fe2
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2021-10-24 17:16:20 +0200

    2021.10.24

--- SAP_MJR.user.js
+++ SAP_MJR.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SAP_MJR
 // @namespace    http://tampermonkey.net/
-// @version      2021.10.23.2
+// @version      2021.10.24
 // @description  try to take over the world!
 // @author       Piet2001 & LSS-Manager
 // @match        https://www.meldkamerspel.com/missions/*
@@ -11,9 +11,9 @@
 
 (async function () {
     'use strict';
-    var versie = "2021.10.23.2"
+    var versie = "2021.10.24"
     if (!localStorage.SAP_MJR_VERSION || JSON.parse(localStorage.SAP_MJR_VERSION).Version !== versie) {
-        var updates = "- Fix voor missie variaties\n- Bericht bij update"
+        var updates = "- Geplande inzetten vanaf 10k in de chat"
 
         alert(`SAP_MRJ - Versie ${versie} nieuwe update! \n\n Updates:\n${updates}`)
 
@@ -32,8 +32,8 @@
     let alliance_chat_credits = 5000; // aantal credits wanneer die in de chat moet worden geplaatst
 
     let planned_chat_setting = false; // Instelling of geplande inzetten standaard in de chat komen
-    let planned_alliance_chat_credits_setting = false; // Alleen in chat plaatsen als boven ingesteld aantal credits. Deze instelling overschrijft de vorige instelling.
-    let planned_alliance_chat_credits = 5000; // aantal credits wanneer geplande inzetten in de chat moet worden geplaatst
+    let planned_alliance_chat_credits_setting = true; // Alleen in chat plaatsen als boven ingesteld aantal credits. Deze instelling overschrijft de vorige instelling.
+    let planned_alliance_chat_credits = 10000; // aantal credits wanneer geplande inzetten in de chat moet worden geplaatst
 
     const getFillTime = () => {
         let time = new Date();
