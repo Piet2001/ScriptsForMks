2a701a93d8fe2e10ab2ad7924685ac3400c25d7c
Piet2001 <piet2001@fireman.net>
2021-11-02 22:58:28 +0100

    Add alliance_cell_costs

---
+++ consolescripts/alliance_cell_costs/alliance_cell_costs_40.js
@@ -0,0 +1,20 @@
+let buildings = await $.getJSON('/api/alliance_buildings', { _: new Date().getTime() })
+buildings = buildings.filter(b => b.building_type === 12)
+let costs = [];
+buildings.forEach(b => {
+    if (b.alliance_share_credits_percentage !== 40) costs.push(b.id)
+})
+
+procesOpenCosts()
+
+async function procesOpenCosts() {
+    let ID = open.length !== 0 ? open[0] : costs.length !== 0 ? costs[0] : 0
+    console.log(open.length + costs.length)
+    if (ID === 0) return;
+    else {
+        await fetch(`/buildings/${ID}/alliance_costs/4`).then(() => {
+            costs.splice(0, 1)
+            procesOpenCosts();
+        })
+    }
+}
