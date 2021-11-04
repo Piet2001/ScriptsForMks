a09a3d4904213d7c3a39ea2e7cadd32c1fa033e7
Piet2001 <piet2001@fireman.net>
2021-11-04 22:08:42 +0100

    2021.11.04

---
+++ consolescripts/build/build_beds_to_30.js
@@ -0,0 +1,20 @@
+let buildings = await $.getJSON('/api/buildings', { _: new Date().getTime() })
+buildings = buildings.filter(b => b.building_type === 2)
+let toExpand = [];
+buildings.forEach(b => {
+    if (b.level !== 20) toExpand.push(b.id)
+})
+
+procesBuy()
+
+async function procesBuy() {
+    let ID = toExpand.length !== 0 ? toExpand[0] : toExpand.length !== 0 ? toExpand[0] : 0
+    console.log(toExpand.length + toExpand.length)
+    if (ID === 0) return;
+    else {
+        await fetch(`/buildings/${ID}/expand_do/credits?level=19`).then(() => {
+            toExpand.splice(0, 1)
+            procesBuy();
+        })
+    }
+}
