5ed5381cb4687110cb9346a0b4b8f9dee1433004
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2023-06-02 12:19:51 +0200

    Create All_extensions_on.js

---
+++ consolescripts/in_of_uitschakelen_gebouwen/All_extensions_on.js
@@ -0,0 +1,15 @@
+await fetch('/api/buildings')
+            .then(response => response.json())
+            .then(data => {
+                buildings = data
+                data.forEach(async function (building) {
+                    if (building.extensions.length > 0) {
+                        building.extensions.forEach(async function (extension) {
+                            if (extension.enabled === false) {
+                                $.ajax({ url: `https://www.meldkamerspel.com/buildings/${building.id}/extension_ready/${extension.type_id}/${building.id}` });
+                                console.log(`${building.caption} - ${extension.caption} ingeschakeld`)
+                            }
+                        })
+                    }
+                })
+            })
