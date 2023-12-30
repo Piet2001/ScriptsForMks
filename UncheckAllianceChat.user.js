f68309ccd93bbe649903ca57a01cd834c4c602c3
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2023-12-30 15:27:37 +0100

    2023.12.30

--- UncheckAllianceChat.user.js
+++ UncheckAllianceChat.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Uncheck alliance chat
 // @namespace    http://tampermonkey.net/
-// @version      2021.11.08
+// @version      2023.12.30
 // @description  try to take over the world!
 // @author       Piet2001
 // @match        https://www.meldkamerspel.com/missions/*
+// @match        https://politie.meldkamerspel.com/missions/*
 // @grant        none
 // ==/UserScript==
 
